5f32223d1476a75901e1877365b34931a373a20f
Aleksi Kumpuniemi <aleksi.kumpuniemi@helsinki.fi>
2022-11-17 11:30:20 +0200

    fly.io setup

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from os import getenv
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
-app.secret_key=getenv("SECRET_KEY")
+app.secret_key = getenv("SECRET_KEY")
 db = SQLAlchemy(app)
 
 @app.route("/")
