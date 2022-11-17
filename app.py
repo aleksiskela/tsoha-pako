fd0734265ee49a70cdae19bb4a01632c20b5d573
Aleksi Kumpuniemi <aleksi.kumpuniemi@helsinki.fi>
2022-11-17 12:35:04 +0200

    fly.io compatibility update

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace("://", "ql://", 1)
+# app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+    
 app.secret_key = getenv("SECRET_KEY")
 db = SQLAlchemy(app)
 
