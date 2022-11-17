4e6aa2a00785a382dad9f1a57b414643961a0d93
Aleksi Kumpuniemi <aleksi.kumpuniemi@helsinki.fi>
2022-11-17 15:52:19 +0200

    login and reg improved. create event added.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template, request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 
@@ -15,6 +16,7 @@ db = SQLAlchemy(app)
 def index():
     sql = db.session.execute("SELECT id, name FROM events")
     events = sql.fetchall()
+    print("Testi")
     return render_template("index.html", events=events)
 
 @app.route("/event/<int:event_id>")
@@ -26,9 +28,20 @@ def show_event(event_id):
 @app.route("/login", methods=["POST"])
 def login():
     username = request.form["username"]
-    # password = request.form["password"]
+    password = request.form["password"]
+    sql = "SELECT id, password FROM users WHERE username=:username"
+    result = db.session.execute(sql, {"username": username})
+    user = result.fetchone()
 
-    session["username"] = username
+    if user:
+        hash_value = user.password
+        if check_password_hash(hash_value, password):
+            session["username"] = username
+        else:
+            return render_template("/login_error.html", message="Username or password incorrect")
+
+    else:
+        return render_template("/login_error.html", message="Username or password incorrect")
 
     return redirect("/")
 
@@ -44,8 +57,61 @@ def register():
     
     if request.method == "POST":
         username = request.form["username"]
-        sql = "INSERT INTO users (username) VALUES (:username)"
-        db.session.execute(sql, {"username": username})
+        password = request.form["password"]
+        password_check = request.form["password_check"]
+
+        sql = "SELECT id FROM users WHERE username=:username"
+        result = db.session.execute(sql, {"username":username})
+        user = result.fetchone()
+
+        if user:
+            print("Username taken")
+            # return redirect("/login_error", message="Username taken")
+            return render_template("login_error.html", message = "Username taken")
+
+        if password != password_check:
+            print("Password mismatch")
+            # return redirect("/login_error")
+            return render_template("login_error.html", message="Password mismatch")
+
+        if not 3 <= len(username) <= 20 or not 3 <= len(password) <= 30:
+            print("Yritit kiertää")
+            # return redirect("/login_error")
+            return render_template("login_error.html", message="Yritit kiertää")
+
+
+        hash_value = generate_password_hash(password)
+
+        sql = "INSERT INTO users (username, password) VALUES (:username, :password)"
+        db.session.execute(sql, {"username": username, "password": hash_value})
         db.session.commit()
 
         return redirect("/")
+
+# @app.route("/login_error")
+# def login_error(message):
+#     return render_template("login_error.html")
+
+@app.route("/create_event", methods=["GET","POST"])
+def create_event():
+    if request.method == "GET":
+        return render_template("create_event.html")
+    
+    if request.method == "POST":
+        name = request.form["name"]
+
+        sql = "SELECT id FROM events WHERE name=:name"
+        result = db.session.execute(sql, {"name":name})
+        event = result.fetchone()
+
+        if event:
+            print("Event name taken")
+            # return redirect("/login_error")
+            return render_template("login_error.html", message="Event name taken")
+
+        else:
+            sql = "INSERT INTO events (name) VALUES (:name)"
+            db.session.execute(sql, {"name":name})
+            db.session.commit()
+    
+    return redirect("/")
